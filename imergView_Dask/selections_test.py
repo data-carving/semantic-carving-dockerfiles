7731baa23f10b17acfbb0b3b75aed6abfcc5e21c
Raffay Atiq <58357644+raffayatiq@users.noreply.github.com>
2025-09-29 16:03:52 -0700

    Update selections_test.py

--- imergView_Dask/selections_test.py
+++ imergView_Dask/selections_test.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 SRI International All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import h5py
 import numpy as np
 import numpy.ma as ma
