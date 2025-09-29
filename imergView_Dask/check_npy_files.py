726356582bc1929526f65ae6d3c04520013ecea3
Raffay Atiq <58357644+raffayatiq@users.noreply.github.com>
2025-09-29 16:02:31 -0700

    Update check_npy_files.py

--- imergView_Dask/check_npy_files.py
+++ imergView_Dask/check_npy_files.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 SRI International All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import os
 import numpy as np
 import glob
