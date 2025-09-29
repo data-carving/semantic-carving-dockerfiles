6c83e00502ce04e132c41acc773f061ff71a5b82
Raffay Atiq <58357644+raffayatiq@users.noreply.github.com>
2025-09-29 16:01:29 -0700

    Update re-execute.sh

--- POMD_PF_SA_Storms/re-execute.sh
+++ POMD_PF_SA_Storms/re-execute.sh
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 SRI International All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 #!/bin/bash
 
 LOG_FILE="/shared/re-execution_results.txt"
